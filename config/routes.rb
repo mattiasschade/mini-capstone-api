facf4cade92e500600037fad5671a7adccb33f4e
Mattias Schade <mattiasschade@gmail.com>
2023-03-30 11:37:56 -0500

    remove non-RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,2 @@
 Rails.application.routes.draw do
-  get "/one_product/:id" => "products#show"
-  get "/all_products" => "products#all"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
