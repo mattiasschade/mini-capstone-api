f65de9123052ed3b7b800b7cb52501543b4659fd
Mattias Schade <mattiasschade@gmail.com>
2023-03-30 12:58:37 -0500

    createt route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,10 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end
+
+  def create
+    @product = Product.new(name: "Spinach", price: 3, image_url: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSMhJ4Gdj8tbvRCcnK5wScleb_Ks6kK78EtNDz1Yz5Z4OQUtzOJaKN4moODiz5-wLbe0oK8NFnS6_vSVtc", description: "Popeye swears by the stuff.")
+    @product.save
+    render :show
+  end
 end
