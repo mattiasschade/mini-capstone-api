68b092bb30e9a37e49224df9f38840456f9d1c21
Mattias Schade <mattiasschade@gmail.com>
2023-04-05 11:31:28 -0500

    seed_dump gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "Apple", price: "3.0", image_url: "https://www.applesfromny.\ncom/wp-content/uploads/2020/05/Fortune_NYAS-Apples2.png", description: "an apple a day keeps the doct\nor away", inventory: nil},
+  {name: "Pineapple", price: "4.0", image_url: "https://images.heb.com/is/image/HEBGrocery/001945935?fit=constrain,1&wid=800&hei=800&fmt=jpg&qlt=85,0&resMode=sharp2&op_usm=1.75,0.3,2,0", description: "a prickly, but delightful, fruit.", inventory: nil},
+  {name: "Salmon", price: "9.0", image_url: "https://res.cloudinary.com/wildasset/image/upload/w_600/c_scale,w_1000/v1657626986/blog/WildAlaskan_RAW_Cropped08.jpg", description: "tasty fish favored by brown bears", inventory: nil},
+  {name: "Eggs", price: "5.0", image_url: "https://img.taste.com.au/F0W1xU4i/taste/2018/02/cls0318eggss_eggs-135792-1.jpg", description: "spawn of chicken", inventory: nil},
+  {name: "Swiss Cheese", price: "4.0", image_url: "https://dfwblobstorage.blob.core.windows.net/ewcmediacontainer/eatwisconsincheese/media/content/cheesemasters-2019/swiss-cheese-header_2.jpg", description: "cheese... but with holes!?", inventory: nil},
+  {name: "Spinach", price: "3.0", image_url: "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSMhJ4Gdj8tbvRCcnK5wScleb_Ks6kK78EtNDz1Yz5Z4OQUtzOJaKN4moODiz5-wLbe0oK8NFnS6_vSVtc", description: "Popeye swears by the stuff.", inventory: nil},
+  {name: "Turtle", price: "50.0", image_url: "https://www.pbs.org/wnet/nature/files/2022/01/GAdUPy5-asset-mezzanine-16x9-fhYslY6.jpg", description: "not too fast.", inventory: nil},
+  {name: "Tomato", price: "2.0", image_url: "https://upload.wikimedia.org/wikipedia/commons/8/89/Tomato_je.jpg", description: "red, shiny, straight from the vine.", inventory: nil}
+])
