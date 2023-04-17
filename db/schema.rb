be9c3de03942b48cab048e3f27f730f11517b83b
Mattias Schade <mattiasschade@gmail.com>
2023-04-17 11:49:29 -0500

    added user signup, broken

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_11_164130) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_163211) do
   create_table "images", force: :cascade do |t|
     t.string "url"
     t.integer "product_id"
@@ -32,6 +32,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_11_164130) do
     t.string "name"
     t.string "email"
     t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
