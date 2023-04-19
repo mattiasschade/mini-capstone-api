70c3838f01330c275258075dab1b1203edac4112
Mattias Schade <mattiasschade@gmail.com>
2023-04-19 12:37:05 -0500

    categories table, category_products table, all linked

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_192523) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_163534) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_products", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "images", force: :cascade do |t|
     t.string "url"
     t.integer "product_id"
